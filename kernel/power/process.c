ea7890cd38fc56834ca183cd33f3bd481887552c
@LeanHijosdesusMadres <105339980+LeanxModulostk@users.noreply.github.com>
2024-10-19 10:07:07 +0200

    kernel: silence suspends/resume logging

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -89,11 +89,11 @@ static int try_to_freeze_tasks(bool user_only)
 
 	if (wakeup) {
 		pr_debug("\n");
-		pr_err("Freezing of tasks aborted after %d.%03d seconds",
+		pr_debug("Freezing of tasks aborted after %d.%03d seconds",
 		       elapsed_msecs / 1000, elapsed_msecs % 1000);
 	} else if (todo) {
 		pr_debug("\n");
-		pr_err("Freezing of tasks failed after %d.%03d seconds"
+		pr_debug("Freezing of tasks failed after %d.%03d seconds"
 		       " (%d tasks refusing to freeze, wq_busy=%d):\n",
 		       elapsed_msecs / 1000, elapsed_msecs % 1000,
 		       todo - wq_busy, wq_busy);
@@ -236,7 +236,7 @@ void thaw_kernel_threads(void)
 	struct task_struct *g, *p;
 
 	pm_nosig_freezing = false;
-	pr_info("Restarting kernel threads ... ");
+	pr_debug("Restarting kernel threads ... ");
 
 	thaw_workqueues();
 
