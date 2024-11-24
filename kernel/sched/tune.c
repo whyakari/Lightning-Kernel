8aadcb481490d4b797e43543cd409c0f98e955fa
@LeanHijosdesusMadres <105339980+LeanxModulostk@users.noreply.github.com>
2024-11-24 19:31:30 +0100

    Delete SchedTune Assist

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1,4 +1,3 @@
-#include <linux/binfmts.h>
 #include <linux/cgroup.h>
 #include <linux/err.h>
 #include <linux/kernel.h>
@@ -678,23 +677,6 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 	return 0;
 }
 
-#ifdef CONFIG_STUNE_ASSIST
-static int boost_write_wrapper(struct cgroup_subsys_state *css,
-			       struct cftype *cft, s64 boost)
-{
-	if (task_is_booster(current))
-		return 0;
-	return boost_write(css, cft, boost);
-}
-static int prefer_idle_write_wrapper(struct cgroup_subsys_state *css,
-				     struct cftype *cft, u64 prefer_idle)
-{
-	if (task_is_booster(current))
-		return 0;
-	return prefer_idle_write(css, cft, prefer_idle);
-}
-#endif
-
 static struct cftype files[] = {
 #ifdef CONFIG_SCHED_WALT
 	{
@@ -711,52 +693,15 @@ static struct cftype files[] = {
 	{
 		.name = "boost",
 		.read_s64 = boost_read,
-#ifdef CONFIG_STUNE_ASSIST
-		.write_s64 = boost_write_wrapper,
-#else
-		.write_u64 = boost_write,
-#endif
+		.write_s64 = boost_write,
 	},
 	{
 		.name = "prefer_idle",
 		.read_u64 = prefer_idle_read,
-#ifdef CONFIG_STUNE_ASSIST
-		.write_u64 = prefer_idle_write_wrapper,
-#else
-		.write_u64 = prefer_idle,
-#endif
+		.write_u64 = prefer_idle_write,
 	},
 	{ }	/* terminate */
 };
-
-#ifdef CONFIG_STUNE_ASSIST
-struct st_data {
-	char *name;
-	int boost;
-	bool prefer_idle;
-};
-
-static void write_default_values(struct cgroup_subsys_state *css)
-{
-	static struct st_data st_targets[] = {
-		{ "audio-app",	0, 0 },
-		{ "background",	0, 0 },
-		{ "foreground",	0, 1 },
-		{ "rt",		0, 0 },
-		{ "top-app",	1, 1 },
-	};
-	int i;
-	for (i = 0; i < ARRAY_SIZE(st_targets); i++) {
-		struct st_data tgt = st_targets[i];
-		if (!strcmp(css->cgroup->kn->name, tgt.name)) {
-			boost_write(css, NULL, tgt.boost);
-			prefer_idle_write(css, NULL, tgt.prefer_idle);
-			pr_debug("stune_assist: setting values for %s: boost=%d prefer_idle=%d\n",
-				tgt.name, tgt.boost, tgt.prefer_idle);
-		}
-	}
-}
-#endif
 
 static int
 schedtune_boostgroup_init(struct schedtune *st)
@@ -793,13 +738,10 @@ schedtune_css_alloc(struct cgroup_subsys_state *parent_css)
 		return ERR_PTR(-ENOMEM);
 	}
 
-	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx) {
+	/* Allow only a limited number of boosting groups */
+	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx)
 		if (!allocated_group[idx])
 			break;
-#ifdef CONFIG_STUNE_ASSIST
-		write_default_values(&allocated_group[idx]->css);
-#endif
-	}
 	if (idx == BOOSTGROUPS_COUNT) {
 		pr_debug("Trying to create more than %d SchedTune boosting groups\n",
 		       BOOSTGROUPS_COUNT);
